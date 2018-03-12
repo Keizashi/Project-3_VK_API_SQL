6f228a4fbeb0e0a7f72ac796c88d104e9ff1b75e
Keizashi <Suhoffpi-88@yandex.ru>
2018-03-12 18:49:10 +0400

    Added DB: test_bd

--- Project-3_VK_API_SQL/settings.py
+++ Project-3_VK_API_SQL/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'test_bd',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'BD_API_10.API_BD.urls'
+ROOT_URLCONF = 'Project-3_VK_API_SQL.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'BD_API_10.wsgi.application'
+WSGI_APPLICATION = 'Project-3_VK_API_SQL.wsgi.application'
 
 
 # Database
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BD_API_10.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project-3_VK_API_SQL.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
