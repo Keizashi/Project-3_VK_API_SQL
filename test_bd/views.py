48744deda8eed10238c7d3ccda7cd95a873851fd
Keizashi <Suhoffpi-88@yandex.ru>
2018-03-15 19:42:00 +0400

    Project done.

--- test_bd/views.py
+++ test_bd/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
+from .models import Note
+import requests
+import json
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('EBANOE DERMO xD')
+    notes_dict = {
+        "data": []
+    }
+
+    all_entries = Note.objects.all().order_by('-id')
+    for entry in all_entries:
+        dict_element = {
+            "id": entry.id,
+            "text": entry.text
+            }
+        notes_dict['data'].append(dict_element)
+    return JsonResponse(notes_dict, safe=False)
